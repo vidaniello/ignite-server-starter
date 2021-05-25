6909995a52135458a54257f4205635313deceacc
Vincenzo D'Aniello <vidaniello@gmail.com>
2021-05-25 16:14:27 +0200

    Added some extra config and new vm argument.

--- bin/boot.bat
+++ bin/boot.bat
@@ -1,14 +1,18 @@
 @echo off
 
-rem ||============================= SETUP =================================||
-rem || MAX_MEMORY           = Max VM memory allocated                      ||
-rem || JRE_HOME             = JVM home, empty for deafault system VM       ||
-rem || BOOTLOADER_HTTP_PORT = Inet sock port of http bootloader commander  ||
-rem ||=====================================================================||
+rem ||============================== SETUP =================================||
+rem || MAX_MEMORY            = Max VM memory allocated                      ||
+rem || JRE_HOME              = JVM home, empty for deafault system VM       ||
+rem || BOOTLOADER_HTTP_PORT  = Inet sock port of http bootloader commander  ||
+rem || IGNITE_NODE_PROP_FILE = Property file name, refer for file to        ||
+rem ||                         execution dir, but can specify URL like      ||
+rem ||                         'http://domain/file.properties'              ||
+rem ||======================================================================||
 
 set "MAX_MEMORY=4g"
 set "JRE_HOME=path_of_JRE"
 set "BOOTLOADER_HTTP_PORT=34445"
+set "IGNITE_NODE_PROP_FILE=ignite_node.properties"
 
 
 set "_HTTP_BASE=http://localhost:%BOOTLOADER_HTTP_PORT%/"
@@ -25,8 +29,9 @@ goto doHelp
 :doStart
 set "_JAVA_BIN_EXEC=%JRE_HOME%\bin\java"
 if "%JRE_HOME%"=="" (set "_JAVA_BIN_EXEC=java")
-set "_COMMAND_LINE_ARG=-Xmx%MAX_MEMORY% -DIGNITE_PERFORMANCE_SUGGESTIONS_DISABLED=true -XX:+UseG1GC -Xms512m -XX:+DisableExplicitGC"
-%_JAVA_BIN_EXEC% %_COMMAND_LINE_ARG% -jar ${enterPoint}.jar %BOOTLOADER_HTTP_PORT%
+set "_COMMAND_LINE_ARG=-Xmx%MAX_MEMORY% -DIGNITE_PERFORMANCE_SUGGESTIONS_DISABLED=true -XX:+UseG1GC -Xms512m -XX:+DisableExplicitGC -Dfile.encoding=UTF-8 -DIGNITE_NODE_PROP_FILE=%IGNITE_NODE_PROP_FILE%"
+"%_JAVA_BIN_EXEC%" %_COMMAND_LINE_ARG% -jar ${enterPoint}.jar %BOOTLOADER_HTTP_PORT%
+timeout /t 1
 goto end
 
 
--- bin/boot.sh
+++ bin/boot.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
 
-# ||============================= SETUP =================================||
-# || MAX_MEMORY           = Max VM memory allocated                      ||
-# || JRE_HOME             = JVM home, empty for deafault system VM       ||
-# || BOOTLOADER_HTTP_PORT = Inet sock port of http bootloader commander  ||
-# ||=====================================================================||
+# ||============================== SETUP =================================||
+# || MAX_MEMORY            = Max VM memory allocated                      ||
+# || JRE_HOME              = JVM home, empty for deafault system VM       ||
+# || BOOTLOADER_HTTP_PORT  = Inet sock port of http bootloader commander  ||
+# || IGNITE_NODE_PROP_FILE = Property file name, refer for file to        ||
+# ||                         execution dir, but can specify URL like      ||
+# ||                         'http://domain/file.properties'              ||
+# ||======================================================================||
 
 export "MAX_MEMORY=4g"
-export "JRE_HOME=../jre1.8.0_291"
+export "JRE_HOME=path_of_JRE"
 export "BOOTLOADER_HTTP_PORT=34445"
+export "IGNITE_NODE_PROP_FILE=ignite_node.properties"
 
 
 export "_HTTP_BASE=http://localhost:$BOOTLOADER_HTTP_PORT/"
@@ -21,7 +25,7 @@ case $1 in
 		then
 			export "_JAVA_BIN_EXEC=java"
 		fi
-		export "_COMMAND_LINE_ARG=-Xmx$MAX_MEMORY -DIGNITE_PERFORMANCE_SUGGESTIONS_DISABLED=true -XX:+UseG1GC -Xms512m -XX:+DisableExplicitGC"
+		export "_COMMAND_LINE_ARG=-Xmx$MAX_MEMORY -DIGNITE_PERFORMANCE_SUGGESTIONS_DISABLED=true -XX:+UseG1GC -Xms512m -XX:+DisableExplicitGC -Dfile.encoding=UTF-8 -DIGNITE_NODE_PROP_FILE=$IGNITE_NODE_PROP_FILE"
 		$_JAVA_BIN_EXEC "$_COMMAND_LINE_ARG" -jar ${enterPoint}.jar "$BOOTLOADER_HTTP_PORT"
 		;;
 	stop)
