f04172238a074e6b70eb4ffcd161d5c918f74c69
Vincenzo D'Aniello <vidaniello@gmail.com>
2021-05-27 16:27:56 +0200

    Some tests

--- src/test/java/com/github/vidaniello/igniteserver/ClientTest.java
+++ src/test/java/com/github/vidaniello/igniteserver/ClientTest.java
@@ -2,6 +2,9 @@ package com.github.vidaniello.igniteserver;
 
 import org.apache.ignite.Ignite;
 import org.apache.ignite.Ignition;
+import org.apache.ignite.client.ClientAddressFinder;
+import org.apache.ignite.client.IgniteClient;
+import org.apache.ignite.configuration.ClientConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
 import org.apache.ignite.spi.discovery.tcp.ipfinder.multicast.TcpDiscoveryMulticastIpFinder;
@@ -14,7 +17,7 @@ public class ClientTest {
 	/**
 	 * On ignite started instance, execute this test
 	 */
-	@Test @Ignore
+	@Test //@Ignore
 	public void testOne() {
 		IgniteConfiguration conf = new IgniteConfiguration();
 		conf.setClientMode(true);
@@ -29,6 +32,29 @@ public class ClientTest {
 		
 		try(Ignite igniteClient = Ignition.start(conf);) {
 			
+			boolean cluster_state = igniteClient.cluster().state().active();
+			
+			int i = 0;
+			
+			
+			
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * On ignite started instance, execute this test
+	 */
+	@Test @Ignore
+	public void testThinClient() {
+		ClientConfiguration cc = new ClientConfiguration().setAddresses(/*"172.16.16.90",*/"172.16.16.60").setPartitionAwarenessEnabled(true);
+		
+		try(IgniteClient igniteClient = Ignition.startClient(cc);) {
+			
+			boolean cluster_state = igniteClient.cluster().state().active();
 			
 			int i = 0;
 			
